98309bdba69bd72bdb6ffb358a17241e60326604
Anuraj <atapidas@gmail.com>
2020-04-13 04:33:37 +0100

    added end condition

--- program.py
+++ program.py
@@ -4,6 +4,11 @@ start = time.time()
 timerLength = 24 * 60 * 60
 end = start + timerLength
 while True:
+    if end-time.time() < 0:
+        file = open("timer.txt",'w')
+        file.write('00:00:00')
+        file.close()
+        break
     file = open("timer.txt",'w')
     file.write(time.strftime('%H:%M:%S',time.gmtime(end-time.time())))
     file.close()
